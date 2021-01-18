acd989e12587e7997440d5e1cf3229f0d074a14a
boluo <boluo.cn>
2021-01-18 16:00:37 +0800

    提交

---
+++ shop_client/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./dist"));
+
+app.listen(9999, () => {
+  console.log("web server running at http://127.0.0.1:9999");
+});
